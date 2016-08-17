07209416ef63c15f445cbf60c8bd86967348f1b8
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2016-08-17 15:21:35 +0300

    Дополнение скрипта выгрузки

--- zip.sh
+++ zip.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-(cd dist; zip -r "../spec_apparat_dist_$(date +'%Y-%m-%d_%H-%M-%S').zip" .)
+STAMP=`date +'%Y-%m-%d_%H-%M-%S'`
+NAME=""
+if [ "$1" = "all" ]; then
+  echo -ne "\n==> Export project:\n\n"
+  NAME="spec_apparat_project_${STAMP}.zip"
+  zip -r "${NAME}" . -x "node_modules/*" "bower_components/*" ".git/*" "*.zip" "*.sh"
+else
+  echo -ne "\n==> Export dist:\n\n"
+  NAME="spec_apparat_dist_${STAMP}.zip"
+  (cd dist; zip -r "../${NAME}" .)
+fi
+echo -e "\n==> File created: ${NAME}"
+
+  
